bdbb01d21637da1c4e9ab3c0924d7849c3e499be
YOOSEOK <70445561+dolDolSee@users.noreply.github.com>
2021-09-07 09:13:45 +0000

    operate

--- Algorithm/operate.py
+++ Algorithm/operate.py
@@ -20,3 +20,36 @@ for plan in plans:
   x,y=nx,ny
 print(x, y)
  
+###2###
+N = int(input())
+
+hour=0
+min=0
+sec=0
+count=0
+
+for h in range(N+1):
+  for m in range(60):
+    for s in range(60):
+      if "3" in str(h) + str(m) + str(s):
+        count +=1
+
+print(count)
+
+###3###
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0]))-int(ord("a")) + 1
+
+steps = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+
+result = 0
+
+for step in steps:
+  next_row = row + step[0]
+  next_col = column + step[1]
+  if next_row <1 or next_row >8 or next_col < 1 or next_col>8:
+    continue
+  else:
+    result +=1
+print(result)
